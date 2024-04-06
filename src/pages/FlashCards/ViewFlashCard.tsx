12906666cf2e20e1f482e21c5dfb49fbe9bbbb6b
pawannitt-26 <pawan.nitt26@gmail.com>
2024-04-06 15:54:51 +0530

    feat: feature added

---
+++ src/pages/FlashCards/ViewFlashCard.tsx
@@ -0,0 +1,22 @@
+import Container from "./Container";
+
+const ViewFlashCard = () => {
+    return (
+        <div style={{display:"flex",flexDirection:"column",gap:"5%"}}>
+            <div style={{ display: "flex",gap:"2%" }}>
+               <Container></Container>
+               <Container></Container>
+               <Container></Container>
+               <Container></Container>
+            </div>
+            <div style={{ display: "flex",gap:"2%" }}>
+               <Container></Container>
+               <Container></Container>
+               <Container></Container>
+               <Container></Container>
+            </div>
+        </div>
+    );
+}
+
+export default ViewFlashCard;
